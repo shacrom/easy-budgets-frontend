f37738d1e239592b6189fbe7f9dc2c3e760b7863
Marcos Miguel <shacrommb@gmail.com>
2025-11-11 19:15:04 +0100

    feat: add environment configuration files for Supabase integration

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://dbqwvbwvqsnihrdczpxs.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRicXd2Ynd2cXNuaWhyZGN6cHhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4NzA0ODksImV4cCI6MjA3ODQ0NjQ4OX0.ywYxaDkNhphUynpYEctw8rjavFlBbgt0ZILaGzTeSw0'
+};
