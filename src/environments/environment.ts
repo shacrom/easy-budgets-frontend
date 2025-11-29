f2690c9adfdce775cdfde9c8aa110509b8dd3dbb
Marcos Miguel <shacrommb@gmail.com>
2025-11-29 13:01:04 +0100

    feat: update environment configuration for development with new Supabase URL and storage bucket

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,17 @@
-export const environment = {
+/* export const environment = {
   production: false,
   supabaseUrl: 'https://dbqwvbwvqsnihrdczpxs.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRicXd2Ynd2cXNuaWhyZGN6cHhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4NzA0ODksImV4cCI6MjA3ODQ0NjQ4OX0.ywYxaDkNhphUynpYEctw8rjavFlBbgt0ZILaGzTeSw0',
   supabaseStorageBucket: 'budget-assets'
+}; */
+
+
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://gmuzptwutaxdeekzxdff.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdtdXpwdHd1dGF4ZGVla3p4ZGZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0MTM4ODIsImV4cCI6MjA3OTk4OTg4Mn0.Rarm2Bar2CN6ynuqWgxG7m1zOoLXbbltXuc0Irfdq5Q',
+  supabaseStorageBucket: 'budget-assets-dev'
 };
+
+
+
