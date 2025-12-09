0fa0e50b4114e41154035f5f0eaa18e0acbc0423
Marcos Miguel <shacrommb@gmail.com>
2025-12-09 16:18:02 +0100

    Remove manual migrations

--- database/migrations/20251130_add_valid_until_to_additional_lines.sql
+++
@@ -1,7 +0,0 @@
--- Añadir campo validUntil a BudgetAdditionalLines para indicar la fecha de validez de los descuentos
--- Esta columna es opcional y solo aplica para líneas de tipo 'discount'
-
-ALTER TABLE public."BudgetAdditionalLines"
-ADD COLUMN "validUntil" DATE NULL;
-
-COMMENT ON COLUMN public."BudgetAdditionalLines"."validUntil" IS 'Fecha de validez del descuento (solo aplica para líneas de tipo discount)';
